9f44b1ae4a558ea01ea659a07de5edd6c6414b90
monakaice <monakaice.programing.3791@gmail.com>
2019-09-26 17:17:26 +0900

    update my keymap.

--- keyboards/mint60/keymaps/monakaice/keymap.c
+++ keyboards/mint60/keymaps/monakaice/keymap.c
@@ -27,25 +27,35 @@ extern rgblight_config_t rgblight_config;
 #endif
 
 enum custom_keycodes {
-  RGBRST = SAFE_RANGE
+  RGBRST = SAFE_RANGE,
+  M_VI_I,
+    M_VI_A,
+    M_VI_O,
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,  \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,  \
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
     KC_CAPS,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,   \
-    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,  MO(1),   \
-    KC_ESC,    KC_LCTL,    KC_LGUI,    KC_LALT,    KC_SPC,        KC_RCTL, KC_APPLICATION,  KC_SPC,               KC_LEFT,KC_DOWN,KC_RGHT \
+    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(2),   KC_UP,  KC_DEL,   \
+    KC_GRV,    KC_LCTL,    KC_LGUI,    KC_LALT,    KC_SPC,        KC_RCTL, TO(1),  KC_RSFT,               KC_LEFT,KC_DOWN,KC_RGHT \
   ),
-  [1] = LAYOUT( \
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
-    RGB_TOG,   RGBRST,  RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX,     RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, \
-    _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_PGUP, _______, \
-    XXXXXXX,   _______,    _______,    _______,    XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_END \
-  )
+  [1] = LAYOU( \
+    _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    _______, \
+    XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, M_VI_I, M_VI_O, KC_HOME, KC_PGUP, XXXXXXX, XXXXXXX, \
+    XXXXXXX,     M_VI_A, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_END, KC_PGDN,        _______, \
+    _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, _______, \
+    XXXXXXX,   _______,    _______,    _______,    _______,       _______, TO(0), _______,                   KC_HOME, KC_PGDN, KC_END \
+  ),
+  [2] = LAYOUT( \
+    _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
+    XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        _______, \
+    _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_BSPC, \
+    XXXXXXX,   _______,    KC_APP,    _______,    _______,       _______, XXXXXXX, _______,                   KC_HOME, KC_PGDN, KC_END \
+  ),
 };
 
 // define variables for reactive RGB
@@ -63,6 +73,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
       #endif
       break;
+    case M_VI_I:
+        if (record->event.pressed) {
+            layer_move(0);
+            return false;
+        }
+      break;
+      case M_VI_A:
+          if (record->event.pressed) {
+              tap_code(KC_RGHT);
+              layer_move(0);
+              return false;
+          }
+          break;
+      case M_VI_O:
+          if (record->event.pressed) {
+              tap_code(KC_END);
+              tap_code(KC_ENT);
+              layer_move(0);
+              return false;
+          }
+          break;
   }
   return true;
 }
